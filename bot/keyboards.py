8ccd0512497e489ccc74eb6ffb7e1a205b1f431d
Anatoly Kozmin <anatolii.kozmin@ibox.ru>
2025-10-23 23:14:23 +0300

    Fix req

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,28 +1,28 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 
 def yes_no_kb():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton(text="Да", callback_data="yes"),
            InlineKeyboardButton(text="Нет", callback_data="no"))
     return kb
 
 
 def pair_or_single_kb():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton(text="В паре", callback_data="pair"),
            InlineKeyboardButton(text="Один", callback_data="single"))
     return kb
 
 
 def studying_or_graduated_kb():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton(text="Учится", callback_data="studying"),
            InlineKeyboardButton(text="Закончил", callback_data="graduated"))
     return kb
 
 
 def confirm_kb():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton(text="Подтвердить", callback_data="confirm"),
            InlineKeyboardButton(text="Отмена", callback_data="cancel"))
     return kb
@@ -41,7 +41,7 @@ def back_reply_kb():
 
 
 def review_kb():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton(text="Редактировать ФИО", callback_data="edit_fio"),
         InlineKeyboardButton(text="Редактировать группу", callback_data="edit_group"))
     kb.add(InlineKeyboardButton(text="Редактировать студбилет", callback_data="edit_student_id"))
