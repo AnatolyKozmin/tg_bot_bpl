f0f34cd5681b3227658b6e23db463e453b259e79
Anatoly Kozmin <anatolii.kozmin@ibox.ru>
2025-10-23 23:36:57 +0300

    Fix req 2

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -35,8 +35,8 @@ def back_kb(action="back"):
 
 
 def back_reply_kb():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    kb.add(KeyboardButton(text="◀️ Назад"))
+    # ReplyKeyboardMarkup in aiogram v3 expects 'keyboard' field (list of rows)
+    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Назад")]], resize_keyboard=True, one_time_keyboard=True)
     return kb
 
 
